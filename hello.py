6431e400ff63d2621163986df0e28991ef101789
SnehilBanerjee <snehilbanerji@yahoo.com>
2019-08-11 18:53:20 +0530

    basic functioning of grpc server-client in python

---
+++ hello.py
@@ -0,0 +1,2 @@
+def hello_user(first_name,last_name):
+  return "Hello " + first_name +" "+ last_name + "!"
---
+++ server.py
@@ -0,0 +1,29 @@
+import grpc
+from concurrent import futures
+import time
+from proto import user_pb2
+from proto import user_pb2_grpc
+
+import hello
+
+class HelloServicer(user_pb2_grpc.HelloServicer):
+
+    def hello_user(self, request, context):
+        response = user_pb2.Respose()
+        response.resp = hello.hello_user(request.first_name,request.last_name)
+        return response
+
+server = grpc.server(futures.ThreadPoolExecutor())
+
+user_pb2_grpc.add_HelloServicer_to_server(
+        HelloServicer(), server)
+
+print('Starting server. Listening on port 50051.')
+server.add_insecure_port('[::]:50051')
+server.start()
+
+try:
+    while True:
+        time.sleep(86400)
+except KeyboardInterrupt:
+    server.stop(0)
